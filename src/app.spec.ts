580dcc59938864228d9b5ecb4f43eb2626dede76
Devyn Cyphers <devyn.cyphers@cen.centeva.com>
2021-11-02 09:27:42 -0600

    add playwright

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,5 +1,7 @@
-import { app } from "./app";
+import { expect, test } from '@playwright/test';
 
-test('App works', () => {
-    expect(app()).toEqual('4');
+test('basic test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await expect(title).toHaveText('Playwright');
 });
